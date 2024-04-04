4e57526ccf6644aad37850d6d8c9d8a4ca16e02c
WilliamReahl <165309515+WilliamReahl@users.noreply.github.com>
2024-04-04 15:44:03 -0400

    Update part2_question1.c

--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int my_strcmp (char s1[], char s2[])
+{
+	int i = 0;
+	
+	while (s1[i] == s2[i] && s1[i] != '\0' && s1[i] != '\0')
+	
+		i++;
+		
+	if (s1[i] != s2[i])
+		return 1;
+	else
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return 0;
+		else
+			return 1;
+}
+
+int my_strncmp (char s1[], char s2[])
+{
+	int i = 0;
+	
+	while (s1[i] == s2[i] && s1[i] != '\0' && s1[i] != '\0' && i<2)
+	
+		i++;
+		
+	if (s1[i] != s2[i])
+		return 1;
+	else
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return 0;
+		else
+			return 1;
+}
+
+char *my_strcpy (char s1[], char s2[])
+{
+	int i = 0
+	
+	while (s1[i] && s1[i] != '\0')
+		
+		s2[i] = s1[i];
+		i++;
+}
+
+int my_strcat (char s1[], char s2[])
+{
+	
+}
+
+int my_reverse (char s1[], char s2[])
+{
+	
+}
+
+int main ()
+{
+	char A [32] = "Tuesday";
+	char B [32] = "Thursday";
+	int a;
+	
+	a = my_strcmp (A,B);
+	
+	printf ("%d for %s and %s", a, A, B);
+	
+	printf ("%d for abcd and abcd", my_strcmp ("abcd", "abcd"));
+}
