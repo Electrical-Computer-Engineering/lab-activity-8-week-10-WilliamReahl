f6813bbc016bbdf02d01b291aa8e224c339b147f
WilliamReahl <165309515+WilliamReahl@users.noreply.github.com>
2024-04-04 16:32:12 -0400

    Update part3_question1.c

--- part3_question1.c
+++ part3_question1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void shiftleft (char s1[])
+{
+	char temp = s1[0];
+	int length = strlen(s1[]);
+	
+	for (int i = 0; i < length - 1; i++)
+	{
+		s1[i] = s1 [i +1];
+	}
+	s1[length -1] = temp;
+}
+
+int main ()
+{
+	char s1[] = "HIESE124";
+	int itterations = strlen(s1[]);
+	
+	for (int i = 0; i < itterations; i++)
+	{
+		shiftleft(s1[]);
+		printf("Shifted left string: %s\n", str);
+	}
+	
+	return 0;
+}
