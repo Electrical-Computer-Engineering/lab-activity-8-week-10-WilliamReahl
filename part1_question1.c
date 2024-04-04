c19a87a15dc933482c3e33134497409134f55497
WilliamReahl <165309515+WilliamReahl@users.noreply.github.com>
2024-04-04 14:43:50 -0400

    Update part1_question1.c

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int my_isalpha (char c)
+{
+	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+}
+
+int my_isdigit (char c)
+{
+	return c >= '0' && c <= '9';
+}
+
+int my_isupper (char c)
+{
+	return c >= 'A' && c <= 'Z';
+}
+
+int my_toupper (char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return c - 'a' + 'A';
+	}
+	
+	return c;
+}
+
+int my_tolower (char c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return c - 'A' + 'a';
+	}
+	
+	return c;
+}
